86030d7a20ce0f25fe90ce1b1551f93a5f600d26
hgunstone <hgunstone2022@elam.co.uk>
2023-03-23 12:42:14 +0000

    Drag and drop and import scene asset

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float distance;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z -distance);
+
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        rb.isKinematic = false;
+    }
+}
