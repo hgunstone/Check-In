a5f166543bf6b777eaa4bb38536649be8121075f
hgunstone <hgunstone2022@elam.co.uk>
2023-03-27 15:34:53 +0100

    talkeable programming

--- Assets/Scripts/CheckForTalk.cs
+++ Assets/Scripts/CheckForTalk.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class CheckForTalk : MonoBehaviour
 {
-
+    Camera cam;
     public bool talkable = false;
-    public LayerMask NPC;
+
+    float rayDistance = 15;
+
+    public float DistanceX = 0.5f;
+    public float DistanceY = 0.5f;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
     void Update()
     {
-        talkable = Physics.Raycast(transform.position, Vector3.forward, 10f, NPC);
+        RaycastHit hit;
+        Ray ray = cam.ViewportPointToRay(new Vector3(DistanceX, DistanceY, 0));
 
-        if(talkable = true)
+        if (Physics.Raycast(ray, out hit, rayDistance))
         {
-            Debug.Log("talkable");
-        }
+            if (hit.collider.CompareTag("NPC"))
+            {
+                Debug.Log("Test");
+
+                talkable = true;
+            }
+            if (hit.collider.CompareTag("Environment"))
+            {
+                talkable = false;
+            }
+        }  
     }
 }
