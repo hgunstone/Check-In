32a240cccbd2412800f0ad83f3e963614443027f
hgunstone <hgunstone2022@elam.co.uk>
2023-05-12 12:48:00 +0100

    Adding more dialogue and adding tutorial page

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -24,4 +24,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Main menu");
     }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
 }
